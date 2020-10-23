5ec01af2964afe6c2c9d982b369772a6f2789c87
Smyrn96 <csd3504@csd.uoc.gr>
2020-10-23 15:49:18 +0300

    Build directory

---
+++ Thesis/build/precache-manifest.a9efe58d06d5e0c9346e67ee2a9a6b46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c9559653b7d0d9b736ac46930a99b8",
+    "url": "/myprojects/index.html"
+  },
+  {
+    "revision": "12143486240aa313df4c",
+    "url": "/myprojects/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f8dab923f0d28e64b3dc",
+    "url": "/myprojects/static/css/main.7f256e91.chunk.css"
+  },
+  {
+    "revision": "12143486240aa313df4c",
+    "url": "/myprojects/static/js/2.872d5c17.chunk.js"
+  },
+  {
+    "revision": "39664d398415d1ee7dfd1a4cabda38e8",
+    "url": "/myprojects/static/js/2.872d5c17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8dab923f0d28e64b3dc",
+    "url": "/myprojects/static/js/main.9c65b43d.chunk.js"
+  },
+  {
+    "revision": "60a908f5a94f7701ac3a",
+    "url": "/myprojects/static/js/runtime-main.885ccdc3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myprojects/static/media/logo.5d5d9eef.svg"
+  }
+]);
